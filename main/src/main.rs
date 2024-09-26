11c47076cfece01a5b253e7b7a86a9fa38a78015
kairoski03 <kairoski03@gmail.com>
2024-09-26 14:59:28 +0900

    Write deployment and activation transaction hex data to a file

--- main/src/main.rs
+++ main/src/main.rs
@@ -77,6 +77,8 @@ enum Apis {
     /// Activate an already deployed contract.
     #[command(visible_alias = "a")]
     Activate(ActivateConfig),
+    /// Generate Activation Tx.
+    ActivationTx(ActivationTxConfig),
     #[command(subcommand)]
     /// Cache a contract using the Stylus CacheManager for Arbitrum chains.
     Cache(Cache),
@@ -190,6 +192,16 @@ pub struct ActivateConfig {
 }
 
 #[derive(Args, Clone, Debug)]
+pub struct ActivationTxConfig {
+    /// Deployed Stylus contract address to activate.
+    #[arg(long)]
+    address: H160,
+    /// The activation transaction hex data file (defaults to stdout).
+    #[arg(long)]
+    output: Option<PathBuf>,
+}
+
+#[derive(Args, Clone, Debug)]
 pub struct CheckConfig {
     #[command(flatten)]
     common_cfg: CommonConfig,
@@ -199,6 +211,9 @@ pub struct CheckConfig {
     /// Where to deploy and activate the contract (defaults to a random address).
     #[arg(long)]
     contract_address: Option<H160>,
+    /// The deployment transaction hex data file.
+    #[arg(long)]
+    output: Option<PathBuf>,
 }
 
 #[derive(Args, Clone, Debug)]
@@ -453,6 +468,12 @@ async fn main_impl(args: Opts) -> Result<()> {
             run!(
                 activate::activate_contract(&config).await,
                 "stylus activate failed"
+            );
+        }
+        Apis::ActivationTx(config) => {
+            run!(
+                activate::write_activation_tx(&config).await,
+                "stylus activation tx writing failed"
             );
         }
         Apis::Cgen { input, out_dir } => {
