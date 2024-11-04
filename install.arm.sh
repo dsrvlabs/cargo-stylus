d21881636938e1900f08974330c6e4eba210f12c
Mark_Jeong <81726531+markjung96@users.noreply.github.com>
2024-11-04 18:45:08 +0900

    feat: add v0.5.2

--- install.arm.sh
+++ install.arm.sh
@@ -3,14 +3,19 @@
 cargo fmt
 cargo clippy --package cargo-stylus --package cargo-stylus-example
 
-# v0.5.3 설치
-git checkout v0.5.3-welldone
-cargo install --path main --root ~/.cargo/v0.5.3-welldone
-
 # v0.5.1 설치
 git checkout v0.5.1-welldone
 cargo install --path main --root ~/.cargo/v0.5.1-welldone
 
+# v0.5.2 설치
+git checkout v0.5.2-welldone
+cargo install --path main --root ~/.cargo/v0.5.2-welldone
+
+# v0.5.3 설치
+git checkout v0.5.3-welldone
+cargo install --path main --root ~/.cargo/v0.5.3-welldone
+
 # 설치된 경로 출력
-echo "v0.5.3-welldone: ~/.cargo/v0.5.3-welldone/bin/cargo-stylus"
 echo "v0.5.1-welldone: ~/.cargo/v0.5.1-welldone/bin/cargo-stylus"
+echo "v0.5.2-welldone: ~/.cargo/v0.5.1-welldone/bin/cargo-stylus"
+echo "v0.5.3-welldone: ~/.cargo/v0.5.3-welldone/bin/cargo-stylus"
